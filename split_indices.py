573e9d35a51b77099bf358db53a2f28de42a3a59
sjoerdvandenbos <sjoerdrvandenbos@gmail.com>
2021-09-08 10:12:01 +0200

    Add split_indices.py

---
+++ split_indices.py
@@ -0,0 +1,24 @@
+import glob
+
+import pandas as pd
+import numpy as np
+
+
+# Training and test data can be divided by sorting
+all_files = sorted(glob.glob("split/ptb-images-2-cropped/*/*/*"))
+first_train, first_test = None, None
+for i, f in enumerate(all_files):
+    if first_train is None and "train" in f:
+        first_train = i
+    # Test folder is called val
+    if first_test is None and "val" in f:
+        first_test = i
+print(f"first train index: {first_train}")
+print(f"first test index: {first_test}")
+
+indices = np.arange(0, len(all_files))
+training = pd.DataFrame(indices[:first_test])
+testing = pd.DataFrame(indices[first_test:])
+
+training.to_csv("split/ptb-images-2-cropped/validation", index=False, header=False)
+testing.to_csv("split/ptb-images-2-cropped/test", index=False, header=False)
