f4fa9bc6951d990dd07d083612e3075c6f9307c0
sjoerdvandenbos <sjoerdrvandenbos@gmail.com>
2021-09-10 15:35:35 +0200

    Complete simultaneous cutting of csv and image datasets

--- cut_dataset.py
+++ cut_dataset.py
@@ -1,13 +1,35 @@
 import glob
 import os
+import numpy as np
 
 
-disease_dirs = glob.glob("split/ptb-images-2-cropped/*/*")
+def reduce_size(zipped, number_every_disease):
+    """
+    @param zipped: A zipped list with tuples of the same disease dirs from image and csv respectively.
+    @param number_every_disease: number of samples to retain in every disease folder.
+    """
+    for image_dir, csv_dir in zipped:
+        images = sorted(glob.glob(f"{image_dir}/*.jpg"))
+        csvs = sorted(glob.glob(f"{csv_dir}/*.csv"))
 
-for d in disease_dirs:
-    all_files = sorted(glob.glob(f"{d}/*"))
-    print(len(all_files))
-    # Remove the first 1000 files we do not want to delete
-    to_delete = all_files[10000:]
-    for f in to_delete:
-        os.remove(f)
+        rand_indices = np.arange(0, len(csvs))
+        np.random.shuffle(rand_indices)
+        indices_to_delete = rand_indices[number_every_disease:]
+
+        for i in indices_to_delete:
+            os.remove(images[i])
+            os.remove(csvs[i])
+
+        size_image_dir = len(glob.glob(f"{image_dir}/*.jpg"))
+        size_csv_dir = len(glob.glob(f"{csv_dir}/*.csv"))
+        print(f"{size_image_dir} files in the img dir, {size_csv_dir} files in the csv dir.")
+
+
+if __name__ == "__main__":
+    train_image_dirs = sorted(glob.glob("split/ptb-images-2-cropped/train/*"))
+    train_csv_dirs = sorted(glob.glob("split/ptb-matrices/train/*"))
+    test_image_dirs = sorted(glob.glob("split/ptb-images-2-cropped/val/*"))
+    test_csv_dirs = sorted(glob.glob("split/ptb-matrices/val/*"))
+
+    reduce_size(zip(train_image_dirs, train_csv_dirs), 3000)
+    reduce_size(zip(test_image_dirs, test_csv_dirs), 1000)
